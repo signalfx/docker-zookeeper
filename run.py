0ef788f7dfcbf1b9f9d88c89779f7698ab2810d8
Maxime Petazzoni <max@signalfuse.com>
2013-10-29 19:20:28 -0700

    Standardize path with abspath
    Signed-off-by: Maxime Petazzoni <max@signalfuse.com>

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ import os
 import sys
 
 os.chdir(os.path.join(
-    os.path.dirname(__file__),
+    os.path.dirname(os.path.abspath(__file__)),
     '..'))
 
 ZOOKEEPER_CONFIG_FILE = 'conf/zoo.cfg'
