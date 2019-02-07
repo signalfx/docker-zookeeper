cbbdbf696c49cbf0fb9f569e4d468fa206c6a441
Marcin Sawicki <msawicki@signalfx.com>
2019-02-07 14:29:43 -0800

    Extract zk server id from hostname
    The myid file is created later, so can't use it here

--- postStart.sh
+++ postStart.sh
@@ -3,7 +3,7 @@
 # Issue a reconfig command to the first node in the cluster (server.1)
 # to register ourselves with the ensemble
 
-MY_ID=server.$(cat /var/lib/zookeeper/myid)
+MY_ID=server.$((${POD_NAME##*-}+1))
 MY_FQDN=$POD_NAME.$DISCOVERY_SERVICE_NAME.$NAMESPACE.svc.cluster.local
 ZK_SERVER_ONE_FQDN=$(echo config | nc localhost 2181 | grep server.1 | cut -d"=" -f2 | cut -d":" -f1)
 echo "reconfig -add $MY_ID=$MY_FQDN:2888:3888:participant;0.0.0.0:2181" | nc $ZK_SERVER_ONE_FQDN 2181
--- preStop.sh
+++ preStop.sh
@@ -3,6 +3,6 @@
 # Issue a reconfig command to the first node in the cluster (server.1)
 # to remove ourselves from the ensemble
 
-ID=$(cat /var/lib/zookeeper/myid)
+ID=$((${POD_NAME##*-}+1))
 ZK_SERVER_ONE_FQDN=$(echo config | nc localhost 2181 | grep server.1 | cut -d"=" -f2 | cut -d":" -f1)
 echo "reconfig -remove $ID" | nc $ZK_SERVER_ONE_FQDN 2181
