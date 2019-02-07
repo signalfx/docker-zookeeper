b7a4e2310dfb09a8ad4f36847d39b96dabc778e2
Marcin Sawicki <msawicki@signalfx.com>
2019-02-07 14:59:09 -0800

    Fix the pre/post hooks
    Back away if ensemble not healthy

--- postStart.sh
+++ postStart.sh
@@ -6,4 +6,6 @@
 MY_ID=server.$((${POD_NAME##*-}+1))
 MY_FQDN=$POD_NAME.$DISCOVERY_SERVICE_NAME.$NAMESPACE.svc.cluster.local
 ZK_SERVER_ONE_FQDN=$(echo config | nc localhost 2181 | grep server.1 | cut -d"=" -f2 | cut -d":" -f1)
-echo "reconfig -add $MY_ID=$MY_FQDN:2888:3888:participant;0.0.0.0:2181" | nc $ZK_SERVER_ONE_FQDN 2181
+if [ -z "$ZK_SERVER_ONE_FQDN" ]; then
+  echo "reconfig -add $MY_ID=$MY_FQDN:2888:3888:participant;0.0.0.0:2181" | nc $ZK_SERVER_ONE_FQDN 2181
+fi
--- preStop.sh
+++ preStop.sh
@@ -5,4 +5,6 @@
 
 ID=$((${POD_NAME##*-}+1))
 ZK_SERVER_ONE_FQDN=$(echo config | nc localhost 2181 | grep server.1 | cut -d"=" -f2 | cut -d":" -f1)
-echo "reconfig -remove $ID" | nc $ZK_SERVER_ONE_FQDN 2181
+if [ -z "$ZK_SERVER_ONE_FQDN" ]; then
+  echo "reconfig -remove $ID" | nc $ZK_SERVER_ONE_FQDN 2181
+fi
