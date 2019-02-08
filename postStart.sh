835966597215e9deeaa43c98bc22502b8a9cedcc
Marcin Sawicki <msawicki@signalfx.com>
2019-02-07 16:40:59 -0800

    Always talk to first zk

--- postStart.sh
+++ postStart.sh
@@ -4,8 +4,9 @@
 # to register ourselves with the ensemble
 
 MY_ID=server.$((${POD_NAME##*-}+1))
+ID=${POD_NAME##*-}
 MY_FQDN=$POD_NAME.$DISCOVERY_SERVICE_NAME.$NAMESPACE.svc.cluster.local
-ZK_SERVER_ONE_FQDN=$(echo config | nc localhost 2181 | grep server.1 | cut -d"=" -f2 | cut -d":" -f1)
+ZK_SERVER_ONE_FQDN=${MY_FQDN/$ID/0}
 if [ "$ZK_SERVER_ONE_FQDN" ]; then
   /opt/zookeeper-3.5.4-beta/bin/zkCli.sh -server $ZK_SERVER_ONE_FQDN:2181 reconfig -add $MY_ID=$MY_FQDN:2888:3888:participant\;0.0.0.0:2181
 fi
--- preStop.sh
+++ preStop.sh
@@ -3,8 +3,10 @@
 # Issue a reconfig command to the first node in the cluster (server.1)
 # to remove ourselves from the ensemble
 
-ID=$((${POD_NAME##*-}+1))
-ZK_SERVER_ONE_FQDN=$(echo config | nc localhost 2181 | grep server.1 | cut -d"=" -f2 | cut -d":" -f1)
+MY_ID=$((${POD_NAME##*-}+1))
+ID=${POD_NAME##*-}
+MY_FQDN=$POD_NAME.$DISCOVERY_SERVICE_NAME.$NAMESPACE.svc.cluster.local
+ZK_SERVER_ONE_FQDN=${MY_FQDN/$ID/0}
 if [ "$ZK_SERVER_ONE_FQDN" ]; then
-	/opt/zookeeper-3.5.4-beta/bin/zkCli.sh -server $ZK_SERVER_ONE_FQDN:2181 reconfig -remove $ID
+	/opt/zookeeper-3.5.4-beta/bin/zkCli.sh -server $ZK_SERVER_ONE_FQDN:2181 reconfig -remove $MY_ID
 fi
